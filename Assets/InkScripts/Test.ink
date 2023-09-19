af01544a53e89011e346063285d3542dab3e650b
Revanth Ponna <93521732+revanthponna@users.noreply.github.com>
2023-09-19 12:23:11 -0600

    Feature: Visual Novel Setup with Dialogue and Choices Display

---
+++ Assets/InkScripts/Test.ink
@@ -0,0 +1,15 @@
+EXTERNAL Name(charName)
+
+
+{Name("Alex")}
+Hey! My name is Alex.
+I love solving puzzles and playing chess.
+
+{Name("Player")}
+Hi. My name is Player
+
+* Aside from puzzles, what else are you interested in?
+You selected Choice 1.
+
+* Would you want to play a game of chess with me?
+You selected Choice 2.
---
+++ Assets/Scripts/VisualNovelSetup/ScriptReader.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+using TMPro;
+
+public class ScriptReader : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset _InkJsonFile;
+    private Story _StoryScript;
+
+    public TMP_Text dialogueBox;
+    public TMP_Text nameTag;
+
+    [SerializeField]
+    private GridLayoutGroup choiceHolder;
+
+    [SerializeField]
+    private Button choiceBasePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadStory();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayNextLine();
+        }
+    }
+
+    void LoadStory()
+    {
+        _StoryScript = new Story(_InkJsonFile.text);
+
+        _StoryScript.BindExternalFunction("Name", (string charName) => ChangeName(charName));
+    }
+
+    public void DisplayNextLine()
+    {
+        if(_StoryScript.canContinue) // Checking if there is content to go through
+        {
+            string text = _StoryScript.Continue(); // Gets Next Line
+            text = text?.Trim(); // Removes White Space from the text
+            dialogueBox.text = text; // Displays new text
+        }
+        else if(_StoryScript.currentChoices.Count > 0)
+        {
+            DisplayChoices();
+        }
+        else
+        {
+            dialogueBox.text = "That's all folks"; // Display when there is no text.
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        if (choiceHolder.GetComponentsInChildren<Button>().Length > 0) return; // Checks if Button holder has buttons in it.
+
+        for(int i = 0; i < _StoryScript.currentChoices.Count; i++)
+        {
+            var choice = _StoryScript.currentChoices[i];
+            var button = CreateChoiceButton(choice.text); // creates a choice button
+
+            button.onClick.AddListener(() => OnClickChoiceButton(choice));
+        }
+    }
+
+
+    Button CreateChoiceButton(string text)
+    {
+        // Instantiate the button Prefab
+        var choiceButton = Instantiate(choiceBasePrefab);
+        choiceButton.transform.SetParent(choiceHolder.transform, false);
+
+        // Change the text in the button Prefab.
+        var buttonText = choiceButton.GetComponentInChildren<TMP_Text>();
+        buttonText.text = text;
+
+        return choiceButton;
+    }
+
+
+    void OnClickChoiceButton(Choice choice)
+    {
+        _StoryScript.ChooseChoiceIndex(choice.index);
+        RefreshChoiceView();
+        DisplayNextLine();
+    }
+
+
+    void RefreshChoiceView()
+    {
+        if(choiceHolder != null)
+        {
+            foreach(var button in choiceHolder.GetComponentsInChildren<Button>())
+            {
+                Destroy(button.gameObject);
+            }
+        }
+    }
+
+
+    public void ChangeName(string name)
+    {
+        string SpeakerName = name;
+
+        nameTag.text = SpeakerName;
+    }
+}
